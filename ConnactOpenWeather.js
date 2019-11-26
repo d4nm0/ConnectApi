690502be92e41d6aa6f10d978be03f1220d7c3d3
d4nm0 <dmonceau@laposte.net>
2019-11-26 16:52:05 +0100

    Add files via upload

---
+++ ConnactOpenWeather.js
@@ -0,0 +1,13 @@
+require('isomorphic-fetch');
+
+let querystring = require('querystring');
+let HttpProxyAgent = require('http-proxy-agent');
+let data = { q:'Orleans,FR', APPID:'2c457ea00970309155f1bffab9b3878d', units:'metric'};
+
+let url="http://api.openweathermap.org/data/2.5/weather?"+querystring.stringify(data);
+
+fetch(url ,{ agent:new HttpProxyAgent('http://wwwcache.univ-orleans.fr:3128')})
+        .then(x => x.json())
+        .then(xx => {
+            console.log(xx);
+        });
